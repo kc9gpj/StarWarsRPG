a3551f8b6371d7af76b49f0ddd49f90f259b1707
david  hoffmann <davidhoffmann@davids-MacBook-Pro.local>
2018-06-02 13:33:20 -0500

    none

---
+++ game.js
@@ -0,0 +1,87 @@
+$(document).ready(function() {
+var winCounter = [];
+var lossCounter = [];
+
+let characters = {
+    'yoda': {
+        name: 'yoda',
+        hp: 140,
+        attack: 10,
+        counterAttack: 10
+        // imageURL: 
+    },
+    'luke': {
+        name: 'luke',
+        hp: 125,
+        attack: 15,
+        counterAttack: 13
+
+    },
+    'mace': {
+        name: 'mace',
+        hp: 110,
+        attack: 15,
+        counterAttack: 18
+    },
+    'kit': {
+        name: 'kit',
+        hp: 110,
+        attack: 10,
+        counterAttack: 8,
+    },
+    'kenobi': {
+        name: 'kenobi',
+        hp: 130,
+        attack: 14,
+        counterAttack: 18,
+    },
+    'kyle': {
+        name: 'kyle',
+        hp: 115,
+        attack: 20,
+        counterAttack: 10
+    },
+    'vader': {
+        name: 'vader',
+        hp: 130,
+        attack: 25,
+        counterAttack: 20,
+    },
+    'sidious': {
+        name: 'sideous',
+        hp: 150,
+        attack: 25,
+        counterAttack: 15,
+    },
+    'bane': {
+        name: 'bane',
+        hp: 130,
+        attack: 20,
+        counterAttack: 15,
+    },
+    'maul': {
+        name: 'maul',
+        hp: 115,
+        attack: 10,
+        counterAttack: 20,
+    },
+    'dooku': {
+        name: 'dooku',
+        hp: 110,
+        attack: 15,
+        counterAttack: 15,
+    },
+    'revan': {
+        name: 'revan',
+        hp: 130,
+        attack: 20,
+        counterAttack: 20
+        }
+            }
+
+
+
+
+
+
+});
